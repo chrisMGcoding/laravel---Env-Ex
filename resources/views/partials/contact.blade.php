7fc54fe0beaadbb77ead168ec7efc2d5f4a5bee0
chris De Groef <chris.degroef@gmail.com>
2021-07-14 15:14:41 +0200

    Laravel : Env ex

---
+++ resources/views/partials/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('template/main')
+
+@section('content')
+
+    <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+
+@endsection
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>{{env('APP_NAME')}}</h1>
+    <h1>{{env('APP_SLOGAN')}}</h1>
+</div>
